41552da26e08163d7f7af9eb4c996ea70e2ed79d
Alessandro Mazzoli <alemazzoli97@gmail.com>
2020-11-25 09:08:14 +0100

    First test of GitHubAPI

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+
+const gitApiRoutes = require('./routes/gitApi');
+
 const app = express()
+
 
 
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', gitApiRoutes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
