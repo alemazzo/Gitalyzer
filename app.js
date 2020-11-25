bde7ae704ca199d6ef5b6ac83fc79b52c41ab805
Alessandro Mazzoli <alemazzoli97@gmail.com>
2020-11-26 00:51:59 +0100

    Added ejs, Added loader for user webpage, Added statics to express

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-const express = require('express')
-
+// Modules
+const express = require('express');
+const ejs = require('ejs');
+// Routes
 const gitApiRoutes = require('./routes/gitApi');
 
+// Express app
 const app = express()
 
+// View engine setup 
+app.set('view engine', 'ejs');
+
+// Set static folder
+app.use(express.static('static'));
 
 
-const port = 3000
 
-app.use('/', gitApiRoutes);
 
+
+
+
+
+app.use('/api/', gitApiRoutes);
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.use('/user/:nickname/', (req, res) => {
+    res.render('user', {
+        nickname: req.params.nickname
+    })
+});
+
+const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
