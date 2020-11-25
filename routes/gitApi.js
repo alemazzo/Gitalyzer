59add34e6ef6ed46dbd5972c03da5dba37ed9260
Alessandro Mazzoli <alemazzoli97@gmail.com>
2020-11-26 00:50:18 +0100

    Create USerAPI

--- routes/gitApi.js
+++ routes/gitApi.js
@@ -1,22 +1,54 @@
 const GitHub = require('github-api');
 const apiKey = require('../config').apiKey;
 
-const routes = require('express').Router()
+const routes = require('express').Router();
+// basic auth
+const github = new GitHub({
+    token: apiKey
+});
 
-routes.get('/', (req, res) => {
+function getUser(nickname) {
+    return github.getUser(`${nickname}`);
+}
 
-    // basic auth
-    const gh = new GitHub({
-        token: apiKey
+function getData(func) {
+    func().then(({
+        data
+    }) => {
+        return data;
     });
+}
 
-    const user = gh.getUser('alemazzo');
-    user.listStarredRepos()
-        .then(function ({
-            data: reposJson
-        }) {
-            res.send(reposJson);
-        });
+// GET user
+routes.get('/users/:nickname/', (req, res) => {
+    const user = getUser(req.params.nickname);
+
+    res.send(data);
+});
+
+// GET repos
+routes.get('/users/:nickname/repos/', (req, res) => {
+    const user = getUser(req.params.nickname);
+    res.send(getData(user.listRepos));
+});
+
+
+// GET followers
+routes.get('/users/:nickname/followers/', (req, res) => {
+    const user = getUser(req.params.nickname);
+    res.send(getData(user.listFollowers));
+});
+
+// GET following
+routes.get('/users/:nickname/following/', (req, res) => {
+    const user = getUser(req.params.nickname);
+    res.send(getData(user.listFollowing));
+});
+
+// GET starred Repos
+routes.get('/users/:nickname/starredRepos/', (req, res) => {
+    const user = getUser(req.params.nickname);
+    res.send(getData(user.listStarredRepos));
 });
 
 module.exports = routes
