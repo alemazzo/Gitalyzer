9f42d734cce7e34c246b5c15777d5ca09da99ab5
Alessandro Mazzoli <alemazzoli97@gmail.com>
2020-11-26 22:43:34 +0100

    Added repos api

--- routes/api/repo.js
+++ routes/api/repo.js
@@ -32,4 +32,35 @@ routes.get('/user/:nickname/repos/:repoName/details/', (req, res) => {
 });
 
 
+routes.get('/user/:nickname/repos/:repoName/contributors/', (req, res) => {
+    const repo = getRepo(req.params.nickname, req.params.repoName);
+    repo.getContributors().then(({
+        data
+    }) => {
+        res.send(data);
+    })
+});
+
+routes.get('/user/:nickname/repos/:repoName/branches/', (req, res) => {
+    const repo = getRepo(req.params.nickname, req.params.repoName);
+    repo.listBranches().then(({
+        data
+    }) => {
+        res.send(data);
+    })
+});
+
+routes.get('/user/:nickname/repos/:repoName/commits/:branchName/', (req, res) => {
+    const repo = getRepo(req.params.nickname, req.params.repoName);
+    repo.listCommits({
+        sha: req.params.branchName
+    }).then(({
+        data
+    }) => {
+        res.send(data);
+    })
+});
+
+
+
 module.exports = routes
